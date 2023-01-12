b1c8d013337dfd0ba8fba4a7cddbc5fc401fabe7
allentumbagahan <114333808+allentumbagahan@users.noreply.github.com>
2023-01-12 12:53:15 -0400

    Update BottingScript.js

--- MySunflowerlandBottingAndManager/BottingScript.js
+++ MySunflowerlandBottingAndManager/BottingScript.js
@@ -1,8 +1,14 @@
 /*Go To https://allentumbagahan.github.io/MySunflowerlandBottingAndManager/read.me For More Info*/
-addJavascript('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js', 'head')
+
 
 const addj = new Promise(function(res, err) {
+    addJavascript('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js', 'head')
     addJavascript('https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js', 'head')
+    if (res){
+        res()
+    }else{
+        console.log(err)
+    }
 }).then(function(val){
     addJavascript('https://allentumbagahan.github.io/MySunflowerlandBottingAndManager/init-firebase.js', 'body')
 }).then(function(val){
