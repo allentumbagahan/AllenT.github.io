a36750608241160123455787a96d0963eb9c1ca1
allentumbagahan <114333808+allentumbagahan@users.noreply.github.com>
2023-01-20 03:24:19 -0400

    update

--- MySunflowerlandBottingAndManager/Main.js
+++ MySunflowerlandBottingAndManager/Main.js
@@ -1,3 +1,19 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBC6W49ilLK5mWHsP2MXKyiIjmLyRGkFiQ",
+    authDomain: "allentumbagahan-9610f.firebaseapp.com",
+    databaseURL: "https://allentumbagahan-9610f-default-rtdb.firebaseio.com",
+    projectId: "allentumbagahan-9610f",
+    storageBucket: "allentumbagahan-9610f.appspot.com",
+    messagingSenderId: "702145349166",
+    appId: "1:702145349166:web:5ca10176bf764ad6cd41bf",
+    measurementId: "G-NRS398L5KQ"
+  };
+    // Initialize Firebase
+        firebase.initializeApp(firebaseConfig)
+    // Initialize Cloud Firestore and get a reference to the service
+const db = firebase.firestore();
+
+
 var docAccountList = db.collection("Accounts").doc("AccountList")
 
 function loadToDb(id) {
