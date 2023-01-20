eb6e01db07246d8b72beb813db96962e1cf86161
allentumbagahan <114333808+allentumbagahan@users.noreply.github.com>
2023-01-20 03:42:13 -0400

    new location

--- MySunflowerlandBottingAndManager/Main.js
+++ MySunflowerlandBottingAndManager/Main.js
@@ -1,49 +1,11 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBC6W49ilLK5mWHsP2MXKyiIjmLyRGkFiQ",
-    authDomain: "allentumbagahan-9610f.firebaseapp.com",
-    databaseURL: "https://allentumbagahan-9610f-default-rtdb.firebaseio.com",
-    projectId: "allentumbagahan-9610f",
-    storageBucket: "allentumbagahan-9610f.appspot.com",
-    messagingSenderId: "702145349166",
-    appId: "1:702145349166:web:5ca10176bf764ad6cd41bf",
-    measurementId: "G-NRS398L5KQ"
-  };
-    // Initialize Firebase
-        firebase.initializeApp(firebaseConfig)
-    // Initialize Cloud Firestore and get a reference to the service
-const db = firebase.firestore();
-
-
-var docAccountList = db.collection("Accounts").doc("AccountList")
-
-function loadToDb(id) {
-    var docRef = db.collection("Accounts").doc(id);
-
-    docRef.get().then((doc) => {
-        if (doc.exists) {
-            loadData = doc.data()
-            console.log("Document data:", doc.data());
-        } else {
-            // doc.data() will be undefined in this case
-            console.log("No such document!");
-        }
-    }).catch((error) => {
-        console.log("Error getting document:", error);
-    });
-}
-
-function loadAllLands() {
-    docAccountList.get().then((doc) => {
-        if (doc.exists) {
-            RegisteredLands = doc.data()
-            console.log("Document data:", doc.data());
-        } else {
-            // doc.data() will be undefined in this case
-            console.log("No such document!");
-        }
-    }).catch((error) => {
-        console.log("Error getting document:", error);
-    });
-}
-
-loadAllLands()
+const addj = new Promise(function(res, err) {
+    addJavascript('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js', 'head')
+    addJavascript('https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js', 'head')
+    if (res){
+        res()
+    }else{
+        console.log(err)
+    }
+}).then(function(val){
+    addJavascript('https://allentumbagahan.github.io/MySunflowerlandBottingAndManager/MainApi.js', 'body')
+})
