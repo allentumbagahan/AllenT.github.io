7fd527bf8e0f98b0ceff88d1ffed0bcd961b72a6
allentumbagahan <114333808+allentumbagahan@users.noreply.github.com>
2023-01-20 03:19:00 -0400

    Update Main.js

--- MySunflowerlandBottingAndManager/Main.js
+++ MySunflowerlandBottingAndManager/Main.js
@@ -1,6 +1,7 @@
-function loadToDb(id) {
+var docAccountList = db.collection("Accounts").doc("AccountList")
 
-    var docRef = db.collection("Accounts").doc("AccountList");
+function loadToDb(id) {
+    var docRef = db.collection("Accounts").doc(id);
 
     docRef.get().then((doc) => {
         if (doc.exists) {
@@ -15,7 +16,18 @@ function loadToDb(id) {
     });
 }
 
-function loadAll() {
-    All 
+function loadAllLands() {
+    docAccountList.get().then((doc) => {
+        if (doc.exists) {
+            RegisteredLands = doc.data()
+            console.log("Document data:", doc.data());
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch((error) => {
+        console.log("Error getting document:", error);
+    });
 }
-setInterval(loadAll, 20000)
+
+loadAllLands()
