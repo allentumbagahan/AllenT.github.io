2687e858c947ed4ede938a44501d7fed1f66213e
allentumbagahan <114333808+allentumbagahan@users.noreply.github.com>
2023-01-20 06:55:39 -0400

    v0.7017

--- MySunflowerlandBottingAndManager/BottingScript.js
+++ MySunflowerlandBottingAndManager/BottingScript.js
@@ -12,7 +12,7 @@ const addj = new Promise(function(res, err) {
 }).then(function(val){
     addJavascript('https://allentumbagahan.github.io/MySunflowerlandBottingAndManager/init-firebase.js', 'body')
 })
-const version = "v0.7016"
+const version = "v0.7017"
 
 main = document.getElementsByClassName("w-full h-full relative")
 plotToClick = []
