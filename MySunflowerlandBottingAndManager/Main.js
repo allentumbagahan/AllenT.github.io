5764acdcfe9aacc3d83d5f9d0efc9ad10bfc5cb5
allentumbagahan <114333808+allentumbagahan@users.noreply.github.com>
2023-01-20 03:46:48 -0400

    Update Main.js

--- MySunflowerlandBottingAndManager/Main.js
+++ MySunflowerlandBottingAndManager/Main.js
@@ -1,3 +1,11 @@
+function addJavascript(jsname,pos) {
+    var th = document.getElementsByTagName(pos)[0];
+    var s = document.createElement('script');
+    s.setAttribute('type','text/javascript');
+    s.setAttribute('src',jsname);
+    th.appendChild(s);
+    }
+
 const addj = new Promise(function(res, err) {
     addJavascript('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js', 'head')
     addJavascript('https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js', 'head')
