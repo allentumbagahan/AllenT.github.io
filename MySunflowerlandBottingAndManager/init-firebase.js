ee7780007dbbf37d487539ba3bc59a3ca40f79a6
allentumbagahan <114333808+allentumbagahan@users.noreply.github.com>
2023-01-21 12:27:21 -0400

    update before saving

--- MySunflowerlandBottingAndManager/init-firebase.js
+++ MySunflowerlandBottingAndManager/init-firebase.js
@@ -82,6 +82,19 @@
                 }
                 else
                 {
+                    var docRef = db.collection("Accounts").doc("AccountList")
+                    docRef.get().then((doc) => {
+                        if (doc.exists) {
+                            RegisteredLands = doc.data()
+                            console.log("Document data:", doc.data());
+                        } else {
+                            // doc.data() will be undefined in this case
+                            console.log("No such document!");
+                        }
+                    }).catch((error) => {
+                        console.log("Error getting document:", error);
+                    });
+                    
                     console.log('saving')
                     RegisteredLands.list.push(landId)
                     console.log(RegisteredLands.list + 'registered')
