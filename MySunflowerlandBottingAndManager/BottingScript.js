366996e62740da997d58e66341910ff510d16f9b
allentumbagahan <114333808+allentumbagahan@users.noreply.github.com>
2023-01-23 04:11:58 -0400

    Update BottingScript.js

--- MySunflowerlandBottingAndManager/BottingScript.js
+++ MySunflowerlandBottingAndManager/BottingScript.js
@@ -112,6 +112,7 @@ function harvestORplant(){
                 }, 1000);
             }else{
                 console.log("no handle")
+                GetInventory()
             }
         }else{
             console.log("already planted")  
@@ -399,7 +400,7 @@ for (let x=0; x < findinventory.length; x++) {
             }
             ItemsOnTheBox[0][0].children[0].click() // click first seed
             document.querySelector("body > div.fade.modal.show > div > div > div > div > div.absolute.flex > img").click()
-        
+        FindPlotButtton()
     }
     function GetLandId(){
         LandId = window.location.hash
