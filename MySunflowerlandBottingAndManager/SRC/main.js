bf2abcfd78937c38334fc10dbbbe9f253beb379f
allentumbagahan <114333808+allentumbagahan@users.noreply.github.com>
2023-01-23 12:26:23 -0400

    update

--- MySunflowerlandBottingAndManager/SRC/main.js
+++ MySunflowerlandBottingAndManager/SRC/main.js
@@ -28,7 +28,7 @@ const observer = new IntersectionObserver(
       }
     },
     // 👇 Threshold is 100%
-    { rootMargin: '-700px 0px 0px 0px',
+    { rootMargin: '-500px 0px 0px 0px',
       root: null,
       }
   );
