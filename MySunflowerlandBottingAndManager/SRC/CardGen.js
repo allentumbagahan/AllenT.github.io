bd864b3c5711c7898f46a283e7043d375308fcfe
allentumbagahan <114333808+allentumbagahan@users.noreply.github.com>
2023-03-19 08:32:55 -0400

    Update CardGen.js

--- MySunflowerlandBottingAndManager/SRC/CardGen.js
+++ MySunflowerlandBottingAndManager/SRC/CardGen.js
@@ -137,7 +137,7 @@ class card {
 
         this.elem1_5 = document.createElement('button')
         this.elem1_5.setAttribute('id', 'bot')
-        this.elem1_5.setAttribute('onclick', `${this.clickStartBot}`)
+        this.elem1_5.setAttribute('onclick', `clickStartBot(${this.id, this.elem1_5_1})`)
         this.elem1_5.setAttribute("style", "display: block; margin-left: auto; margin-right: auto; border: #5d3e02 solid 2px; border-radius: 20px; --tw-bg-opacity: 1; background-color: rgb(185 111 80 / var(--tw-bg-opacity));")
         this.elem1.appendChild(this.elem1_5)
         this.elem1_5_1 = document.createElement('h3')
@@ -212,19 +212,7 @@ class card {
 
         }
     }
-    clickStartBot(){
-        console.log(this.bot)
-        switch(this.bot){
-            case "Start Bot":
-                this.bot = "Stop Bot"
-                BotSaveToDb(this.id, "Stop Bot")
-                break;
-            case "Stop Bot":
-                this.bot = "Start Bot"
-                BotSaveToDb(this.id, "Start Bot")
-                break;
-        }
-    }
+
 }
 
 function Calculate(){
@@ -287,6 +275,21 @@ function ComputeTotalbal(val) {
     
 }
 
-
 ConnectToApi()
+
+function clickStartBot(id, botElem){
+    console.log(botElem)
+    switch(botElem){
+        case "Start Bot":
+            botElem = "Stop Bot"
+            BotSaveToDb(id, "Stop Bot")
+            console.log(id + " saving " + "Stop Bot")
+            break;
+        case "Stop Bot":
+            botElem = "Start Bot"
+            BotSaveToDb(id, "Start Bot")
+            console.log(id + " saving " + "Start Bot")
+            break;
+    }
+}
 
